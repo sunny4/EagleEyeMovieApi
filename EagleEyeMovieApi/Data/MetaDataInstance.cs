c522cdead7b006e43f30c0f023bb6a274a2acf64
sunny4 <sunny4@blueyonder.co.uk>
2020-11-16 16:56:46 +0000

    Created class MetaDataInstance

---
+++ EagleEyeMovieApi/Data/MetaDataInstance.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EagleEyeMovieApi.Data
+{
+    public class MetaDataInstance
+    {
+        public int Id { get; set; }
+        public int MovieId { get; set; }
+        public string Title { get; set; }
+        public string Language { get; set; }
+        public string Duration { get; set; }
+        public int ReleaseYear { get; set; }
+    }
+}
